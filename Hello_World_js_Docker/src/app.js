bd6e0b932d612a3058b8c8afb7a32a6d9a694f5c
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2023-11-23 12:31:28 +0530

    testing different code

--- Hello_World_js_Docker/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (_req, res) => {
-  console.log("Request received");
-  res.status(200).send("Hello world!")
-
-})
-module.exports = app;
--- Hello_World_js_Docker/src/index.js
+++ Hello_World_js_Docker/src/index.js
@@ -1,8 +1,19 @@
-const app = require("./app");
-const port = 3000;
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
-  console.log(`Go to http://localhost:${port}/`),
+const server = http.createServer((req, res) => {
+  const method = req.method.toUpperCase();
+  res.setHeader('Content-Type', 'application/json');
+  res.writeHead(200, 'request is good');
+  if (method === 'POST') {
+    const response = JSON.stringify({ message: 'Hello World!' });
+    res.end(response);
+  } else {
+    const response = JSON.stringify({ message: 'Welcome Automation' });
+    res.end(response);
+  }
+});
 
-);
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});

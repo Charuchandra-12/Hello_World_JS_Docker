bd975497184914d739473663890a1d6a11b9a031
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2023-11-26 10:29:51 +0530

    added working code

--- Hello_World_js_Docker/src/app.js
+++ Hello_World_js_Docker/src/app.js
@@ -1,9 +1,12 @@
-const express = require("express");
-const app = express();
+const http = require('http');
 
-app.get("/", (_req, res) => {
-  console.log("Request received");
-  res.status(200).send("Hello world!")
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, ggggg!!!\n');
+});
 
-})
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
